2ce6569645efb3be59a1b806e4932e1f53ad3d20
abijithYayavaram <ayayavaram@ucsd.edu>
2024-01-18 12:18:45 -0800

    Uploading PA2

---
+++ PA2/template.cu
@@ -0,0 +1,64 @@
+#include <gputk.h>
+
+__global__ void vecAdd(float *in1, float *in2, float *out, int len) {
+  //@@ Insert code to implement vector addition here
+}
+
+int main(int argc, char **argv) {
+  gpuTKArg_t args;
+  int inputLength;
+  float *hostInput1;
+  float *hostInput2;
+  float *hostOutput;
+  float *deviceInput1;
+  float *deviceInput2;
+  float *deviceOutput;
+
+  args = gpuTKArg_read(argc, argv);
+
+  gpuTKTime_start(Generic, "Importing data and creating memory on host");
+  hostInput1 =
+      (float *)gpuTKImport(gpuTKArg_getInputFile(args, 0), &inputLength);
+  hostInput2 =
+      (float *)gpuTKImport(gpuTKArg_getInputFile(args, 1), &inputLength);
+  hostOutput = (float *)malloc(inputLength * sizeof(float));
+  gpuTKTime_stop(Generic, "Importing data and creating memory on host");
+
+  gpuTKLog(TRACE, "The input length is ", inputLength);
+
+  gpuTKTime_start(GPU, "Allocating GPU memory.");
+  //@@ Allocate GPU memory here
+
+  gpuTKTime_stop(GPU, "Allocating GPU memory.");
+
+  gpuTKTime_start(GPU, "Copying input memory to the GPU.");
+  //@@ Copy memory to the GPU here
+
+  gpuTKTime_stop(GPU, "Copying input memory to the GPU.");
+
+  //@@ Initialize the grid and block dimensions here
+
+  gpuTKTime_start(Compute, "Performing CUDA computation");
+  //@@ Launch the GPU Kernel here
+
+  cudaDeviceSynchronize();
+  gpuTKTime_stop(Compute, "Performing CUDA computation");
+
+  gpuTKTime_start(Copy, "Copying output memory to the CPU");
+  //@@ Copy the GPU memory back to the CPU here
+
+  gpuTKTime_stop(Copy, "Copying output memory to the CPU");
+
+  gpuTKTime_start(GPU, "Freeing GPU Memory");
+  //@@ Free the GPU memory here
+
+  gpuTKTime_stop(GPU, "Freeing GPU Memory");
+
+  gpuTKSolution(args, hostOutput, inputLength);
+
+  free(hostInput1);
+  free(hostInput2);
+  free(hostOutput);
+
+  return 0;
+}
